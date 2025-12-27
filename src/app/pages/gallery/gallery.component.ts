3e04eae1c304342563e2a127f14a57932b14f766
10431Fahim <fahim.cse.diu@gmail.com>
2025-12-27 23:26:51 +0600

    issue fix

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,175 +1,95 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ViewChild, signal, computed, inject, DestroyRef, OnInit} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { GalleryPopupComponent } from './gallery-popup/gallery-popup.component';
 import {Photo} from '../../interfaces/common/photo.interface';
-import {Subscription} from 'rxjs';
 import {ImageGallery} from '../../interfaces/common/image-gallery';
 import {ImageFolder} from '../../interfaces/common/image-folder';
 import {PhotoService} from '../../services/common/photo.service';
-import {FilterData} from '../../interfaces/core/filter-data';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
-// var mixitup = require('mixitup');
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss'],
+  standalone: true,
+  host: {ngSkipHydration: 'true'},
   imports: [
     GalleryPopupComponent,
-    NgForOf
-  ]})
+    NgForOf,
+    NgIf
+  ]
+})
 export class GalleryComponent implements OnInit {
-  @ViewChild('galleryPop',{static:false}) galleryPop!:GalleryPopupComponent;
-  _albums: any = [];
-    // Store Data
-    photo: any[] = [];
-    // Subscriptions
-    private subDataThree!: Subscription;
+  @ViewChild('galleryPop', {static: false}) galleryPop!: GalleryPopupComponent;
 
-  // Pagination
- currentPage = 1;
- totalProducts = 0;
- productsPerPage = 24;
- totalProductsStore = 0;
+  // Angular 20 Signals for reactive state management
+  photo = signal<Photo[]>([]);
+  isChangeLanguage = signal<boolean>(false);
+  isChangeLanguageToggle = signal<string>('bn');
+  images = signal<ImageGallery[]>([]);
+  folders = signal<ImageFolder[]>([]);
+  selectedImages = signal<Photo[]>([]);
+  selectPreview = signal<Photo | undefined>(undefined);
+  selectedFolder = signal<any>(null);
+  isLoading = signal<boolean>(false);
 
-  // images: ImageGallery[] = [];
-  selectedFolder = null;
-  private holdPrevData: any[] = [];
-  // SELECTED IMAGE
-  selectedImages: Photo[] = [];
-  selectPreview?: Photo;
+  // Pagination signals
+  currentPage = signal<number>(1);
+  totalProducts = signal<number>(0);
+  productsPerPage = signal<number>(24);
+  totalProductsStore = signal<number>(0);
 
+  // Computed signals for derived state
+  hasPhotos = computed(() => this.photo().length > 0);
+  isEnglish = computed(() => this.isChangeLanguage());
+  isBengali = computed(() => !this.isChangeLanguage());
 
-  isChangeLanguage: boolean = false;
-  isChangeLanguageToggle: string = 'bn';
-  images: ImageGallery[] = [];
-  folders: ImageFolder[] = [];
-  // light box
-  // Subscriptions
-  private subscriptions: Subscription[] = [];
-
-  constructor(
-    private photoService: PhotoService,
-    public translateService: TranslateService) {
-
-  }
+  // Angular 20 dependency injection
+  private photoService = inject(PhotoService);
+  private translateService = inject(TranslateService);
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    this.getAllPhotos()
+    this.getAllPhotos();
   }
-
-
-
-
-  // private getAllPhotos() {
-  //   // Select
-  //   const mSelect = {
-  //     name: 1,
-  //     nameEn: 1,
-  //     image: 1,
-  //     images:1,
-  //     _id: 1,
-  //     slug: 1,
-  //     createdAt: 1
-  //   }
-  //
-  //   const filterData: FilterData = {
-  //     pagination: null,
-  //     filter: null,
-  //     select: mSelect,
-  //     sort: { createdAt: -1 }
-  //   }
-  //
-  //   this.subDataThree = this.photoService.getAllPhotos(filterData, null)
-  //     .subscribe({
-  //       next: res => {
-  //         if (res.success) {
-  //           this.photo = res.data;
-  //           if (this.photo && this.photo.length) {
-  //             // this.prepareImagesForLightBox();
-  //           }
-  //         }
-  //       },
-  //       error: error => {
-  //         // console.log(error);
-  //       }
-  //     });
-  // }
 
   private getAllPhotos(): void {
-    const subscription = this.photoService.getAllPhoto()
+    this.isLoading.set(true);
+    this.photoService.getAllPhoto()
+      .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: res => {
-          this.photo = res.data;
-          // this.isLoading = false;
+          this.photo.set(res.data);
+          this.isLoading.set(false);
         },
         error: err => {
-          console.error(err);
-          // this.isLoading = false;
+          console.error('Error loading photos:', err);
+          this.isLoading.set(false);
         }
       });
-    this.subscriptions?.push(subscription);
   }
 
-  //
-  // private prepareImagesForLightBox() {
-  //   this.albums = this.images.map(m => {
-  //
-  //     return {
-  //       src: m.url,
-  //       // caption: m.name,
-  //       thumb: m.url
-  //
-  //     } as IAlbum;
-  //
-  //   });
-  // }
-
-  onChangeLanguage(language: string) {
-    this.isChangeLanguage = language === 'en';
+  onChangeLanguage(language: string): void {
+    this.isChangeLanguage.set(language === 'en');
     this.translateService.use(language);
   }
 
-  onChangeLanguageToggle(language: string){
-     if(this.isChangeLanguageToggle !== language){
-           this.isChangeLanguageToggle = language;
-           this.isChangeLanguage = true;
-           this.translateService.use(this.isChangeLanguageToggle);
-     }
-     else{
-      this.isChangeLanguageToggle = 'bn';
-      this.isChangeLanguage = false;
-      this.translateService.use(this.isChangeLanguageToggle);
-     }
+  onChangeLanguageToggle(language: string): void {
+    if (this.isChangeLanguageToggle() !== language) {
+      this.isChangeLanguageToggle.set(language);
+      this.isChangeLanguage.set(true);
+      this.translateService.use(language);
+    } else {
+      this.isChangeLanguageToggle.set('bn');
+      this.isChangeLanguage.set(false);
+      this.translateService.use('bn');
+    }
   }
 
-  onShowPop(index:any){
-      if(index > -1){
-        this.galleryPop.onShowGallery(index);
-        // console.log(index);
-      }
-  }
-
-    /**
-   * LIGHT BOX VIEW DIALOG
-   */
-  //
-  // openLightBox(index: number): void {
-  //   this.lightboxConfig.showZoom = false;
-  //   this.lightboxConfig.showRotate = false;
-  //   this.lightboxConfig.centerVertically = false;
-  //   this.lightboxConfig.enableTransition = false;
-  //   this.lightbox.open(this.albums, index);
-  // }
-  //
-  // closeLightBox(): void {
-  //   this.lightbox.close();
-  // }
-
-  /**
-   * ON Destroy
-   */
-  ngOnDestroy() {
-    this.subscriptions?.forEach(sub => sub?.unsubscribe());
+  onShowPop(index: number): void {
+    if (index > -1 && this.galleryPop) {
+      this.galleryPop.onShowGallery(index);
+    }
   }
 }
